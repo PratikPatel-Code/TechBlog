9ccda8543b511d6023d4a27b03686d787dc73149
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-12 00:52:56 -0400

    Seperated login and logout into two seperate js files

---
+++ public/js/login.js
@@ -0,0 +1,47 @@
+// Logging In using Form Handler
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Creating a new sign up using Form Handler
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Document commands for login and signup
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
