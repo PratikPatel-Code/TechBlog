0614dc71ad037f7e925ececeb6017e6ba26436ea
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-12 01:05:53 -0400

    Testing document selectors for profile/dashboard aspect

---
+++ public/js/profile.js
@@ -0,0 +1,72 @@
+// Create
+const newFormHandler = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#post-name").value.trim();
+  const contents = document.querySelector("#post-desc").value.trim();
+  if (title && contents) {
+    const response = await fetch(`/api/blogs`, {
+      method: "POST",
+      body: JSON.stringify({ title, contents }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create a new post");
+    }
+  }
+};
+
+// Delete
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id") && event.target.id === "del-btn") {
+    const id = event.target.getAttribute("data-id");
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Error");
+    }
+  }
+};
+
+// Update
+const updateButtonHandler = async (event) => {
+  if (
+    event.target.hasAttribute("data-id") &&
+    event.target.id === "update-btn"
+  ) {
+    const id = event.target.getAttribute("data-id");
+    const title = document.querySelector(`#title-${id}`).value.trim();
+    const contents = document.querySelector(`#blog-${id}`).value.trim();
+
+    console.log(event.target.id);
+    const response = await fetch(`api/blogs/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, contents }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Error");
+    }
+  }
+};
+
+// Document selectors to create, delete, and update
+document.querySelector("#btnNew").addEventListener("click", newFormHandler);
+
+document
+  .querySelector(".delete-list")
+  .addEventListener("click", delButtonHandler);
+
+document
+  .querySelector(".post-list")
+  .addEventListener("click", updateButtonHandler);
