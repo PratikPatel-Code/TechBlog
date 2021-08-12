9961a6d1e15796bd52c8f0677eb74f295b650afa
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-11 22:53:40 -0400

    finished connection.js

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// I copied and pasted the connection.js from the student mini project found in MVC folder since it is exactly the same
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
