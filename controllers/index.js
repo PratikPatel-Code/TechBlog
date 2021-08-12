ba430c27e685d7fd7449018dda22492089b7ea18
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-11 23:36:16 -0400

    finished root files of controllers, API still needs work

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+// Standard require used student mini project mvc as reference.  
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
